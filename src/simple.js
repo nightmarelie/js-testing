786cce4a44367668cc8d3ab42a6cf7c4bd6810f2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-11 10:10:22 -0600

    feat: create global setup for tests

--- src/simple.js
+++ src/simple.js
@@ -19,25 +19,3 @@ test("subtract subtracts numbers", async () => {
 
   expect(result).toBe(expected);
 });
-
-async function test(title, callback) {
-  try {
-    await callback();
-    console.log(`✅ ${title}`);
-  } catch (error) {
-    console.error(`❌ ${title}`);
-    console.error(error);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-    toEqual(expected) {},
-    toBeGreaterThan(expected) {},
-  };
-}
